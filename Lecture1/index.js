f93e8ed2c98785fd6d977423532107de559acb7e
aayushgupta52 <iguptaaayush09@gmail.com>
2026-04-11 22:53:49 +0530

    Initial commit with Lecture1

---
+++ Lecture1/index.js
@@ -0,0 +1,74 @@
+// console.log('hello world')
+// // constant  
+// const a = 12;
+ 
+
+// let lastName = "aayush";
+// console.log(lastName)
+
+
+// lastName = 1
+// console.log(lastName)
+
+// reference types in javascript 
+
+// object in javascript 
+
+
+// let age = 27;
+// let status =(age>=18) ? 'yes vote' : 'no  vote';
+
+// console.log(status);
+
+
+
+// logical operator 
+
+//  let value = (false || 0 || 1)
+// console.log(value)
+
+// let check = (false && 2 && 1)
+// console.log(check)
+
+
+// if else statement 
+
+// let marks = 40;
+
+// if(marks >= 90) {
+//     console.log('A')
+// }
+// else if(marks>= 80) {
+//     console.log('B');
+// }
+// else if(marks>= 70) {
+//     console.log('C');
+// }
+// else if(marks>= 60) {
+//     console.log('D');
+// }
+// else {
+//     console.log('E')
+// }
+
+
+// switch case statement 
+
+// let num = 1;
+
+// switch(num)  {
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3 : console.log('C');
+//     break;
+//     case 4: console.log('D');
+// }
+
+
+// infinite loop 
+
+// while (true) {
+//     console.log("Hello");
+// }
---
+++ Lecture1/loops2.js
@@ -0,0 +1,29 @@
+// loops in javascript 
+
+for(let i = 0 ; i <5 ; i++) {
+    console.log("Aayush" + " " +  i)
+}
+
+
+// while loop syntax 
+
+let x = 1;
+
+while (x<=5) {
+    console.log(x);
+    x++;
+}
+
+// do- while loop 
+
+let y = 1;
+
+do {
+    console.log(y);
+    y++;
+} while(y <= 100) 
+
+
+
+
+
