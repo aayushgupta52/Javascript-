baf293b6a746464609f8e1027c9e8f1a0b081638
aayushgupta52 <iguptaaayush09@gmail.com>
2026-09-09 21:19:50 +0530

    Add Lecture3 JavaScript code

---
+++ Lecture3/array.js
@@ -0,0 +1,77 @@
+// let obj = {
+//     name: 'aayush',
+//     age: 27,
+//     greet: function() {
+//         console.log("hello there");
+//     }
+// }
+// obj.greet() 
+
+
+
+// Array creation in js  
+
+// let arr = [1,2,3,4,5]
+// let brr = new Array(1,"Aayush" , "roshan singh", "jethalal")
+
+// console.log(typeof(arr))
+// console.log(typeof(brr))
+
+// array access
+
+// console.log(brr[1])
+
+//array ki takquat 
+// brr.push("mutual friend");
+// brr.pop()
+// brr.shift(1)
+// brr.unshift("acer")
+// brr.slice(1,2)
+// brr.splice(1,0,"babita")
+// console.log(brr)
+
+
+
+// mapping 
+
+// let arr1 = [10, 20 , 30 ,  40 , 50]
+
+// arr1.map((number,index) => {
+//     console.log(number)
+//     console.log(index)
+
+// })
+
+
+// filter in array 
+
+
+// let arr2 = [1, "Aayush" , "babbar", "shabbar"]
+
+
+// let v = arr2.filter((value)=> {
+//     if(typeof(value) === "string") {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+    
+// });
+
+
+// console.log(v);
+
+
+
+// arr4 = [10 ,20 , 30 , 40]
+
+// let jetengine =  arr4.reduce((acc , curr)=>{
+//     return acc + curr;
+
+// } , 0)
+
+// console.log(jetengine)
+
+
+
